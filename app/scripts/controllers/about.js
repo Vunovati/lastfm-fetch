b5caf430b069afdb55e584c26a9ceaab1be15d29
evlaada <vladimir.adamic@ericsson.com>
2014-07-27 15:28:37 +0200

    First commit - initalized angular project

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name lastfmFetchApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the lastfmFetchApp
+ */
+angular.module('lastfmFetchApp')
+  .controller('AboutCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name lastfmFetchApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the lastfmFetchApp
+ */
+angular.module('lastfmFetchApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
